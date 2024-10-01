bd755d5f9ac45e8ef0cc4d39cd4690ee6665dc35
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-01 14:08:03 +0200

    Create  ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,23 @@
+function getBasketContent() {
+    let fruits = [];
+
+    fruits.push("strawberry");
+    fruits.push("strawberry");
+    fruits.push("strawberry");
+    fruits.push("apple");
+    fruits.push("apple");
+    fruits.push("lime");
+    fruits.push("lime");
+    fruits.push("peach");
+    fruits.push("pear");
+    fruits.push("pear");
+
+    let totaldefruits = fruits.length;
+
+    console.log("Total: " + totaldefruits + " fruits"); 
+    
+    return fruits; 
+}
+
+
+getBasketContent(); 
